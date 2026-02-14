aa87bfd03780108d6c8052ec2b82752a81512cc2
Fussed <diegopalencia19@gmail.com>
2026-02-14 21:04:21 +0100

    Configurar despliegue en GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Fussed19.github.io',
+  base: '/FussedPortfolio',
+  output: 'static',
   integrations: [react()],
 
   vite: {
