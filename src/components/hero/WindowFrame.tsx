558f963719c43a2461b3729e15feddf29a2070f6
Fussed <diegopalencia19@gmail.com>
2026-02-15 11:00:23 +0100

    ventana

--- src/components/hero/WindowFrame.tsx
+++ src/components/hero/WindowFrame.tsx
@@ -26,7 +26,7 @@ export default function WindowFrame({ children, onModelChange, modelIndex }: Win
     if (width < 768) return { width: 200, height: 200 };
     if (width < 1024) return { width: 280, height: 280 };
     if (width < 1280) return { width: 310, height: 310 };
-    if (width < 2880) return { width: 370, height: 370 };
+    if (width < 2200) return { width: 370, height: 370 };
     return { width: 500, height: 500 };
   };
 
